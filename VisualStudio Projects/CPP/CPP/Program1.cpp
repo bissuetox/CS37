715251936435bc9867e874d1f8c7680e4605de2c
bissuetox <bissuetox@gmail.com>
2019-09-26 01:23:05 -0700

    Completed Program1 V2

--- VisualStudio Projects/CPP/CPP/Program1.cpp
+++ VisualStudio Projects/CPP/CPP/Program1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // struct sCrop that contains all of the crop data to be both inputted and calculated
 struct sCrop {
 	string cropName;
-	float costAcre, costBushel, increase, minNP, maxNP, avgNP = 0;
+	float costAcre, costBushel, increase, minNP, maxNP, avgNP = 0; 
 	int yield;
 };
 
@@ -18,31 +18,31 @@ void printResults(sCrop crop);
 // main func 
 int main()
 {
-	sCrop crops[4];
-	sCrop &bestCrop = crops[0];
+	sCrop crops[4]; // array of crops
+	sCrop &bestCrop = crops[0]; // set a reference to the first crop for later iteration
 
 	for (int i = 0; i < 4; i++) 
 	{
 		inputData(crops[i]);
 		calculate(crops[i]);
 		printResults(crops[i]);
-		if (crops[i].avgNP > bestCrop.avgNP)
+		if (crops[i].avgNP > bestCrop.avgNP) // if the iterated crop has a greater average than the current bestCrop, then set that crop to the best Crop
 			bestCrop = crops[i];
 	}
-	cout << "Old MacDonald, you should plant " << bestCrop.cropName << endl << endl;
+	cout << "Old MacDonald, you should plant " << bestCrop.cropName << endl << endl; // print the best crop
 }
 
-// function that calculates the min, max, and average net profit
+// function that calculates the min, max, and average net profit, and saves it in the passed crop struct
 void calculate(sCrop &crop)
 {
-	float minGross = crop.yield * crop.costBushel * 1000;
+	float minGross = crop.yield * crop.costBushel * 1000; 
 	float maxGross = minGross + (minGross * (crop.increase / 100));
 	crop.minNP = minGross - (crop.costAcre * 1000);
 	crop.maxNP = maxGross - (crop.costAcre * 1000);
 	crop.avgNP = (crop.minNP + crop.maxNP) / 2;
 }
 
-// function that prompts the user to input the crop name and data
+// function that prompts the user to input the crop name and data and stores it in the passed crop struct
 void inputData(sCrop &crop)
 {
 	cout << "Enter the crop name: ";
@@ -52,7 +52,7 @@ void inputData(sCrop &crop)
 	cin.ignore();
 }
 
-// function that prints all of the data inside formatted nicely
+// function that prints all of the data inside the passed crop struct and formats it nicely
 void printResults(sCrop crop)
 {
 	cout << left << setw(15) << "";
@@ -60,32 +60,32 @@ void printResults(sCrop crop)
 	cout << setw(20) << "Maximum Profit";
 	cout << setw(20) << "Average Profit" << endl;
 
-	cout << right << setw(15) << crop.cropName + ":  ";
-	cout << left << setprecision(2) << fixed << " $" << setw(19) << crop.minNP;
+	cout << left << setw(15) << crop.cropName;
+	cout << setprecision(2) << fixed << " $" << setw(19) << crop.minNP;
 	cout << "$" << setw(19) << crop.maxNP;
 	cout << "$" << setw(19) << crop.avgNP << endl << endl;
 }
 
 /*
-Enter the crop name: Corn
-Enter cost, yield, price per bushel, and increase data: 41 150 .55 5.5
+Enter the crop name: Sweet corn
+Enter cost, yield, price per bushel, and increase data: 45.25 173 0.54 4.7
 			   Minimum Profit      Maximum Profit      Average Profit
-		Corn:   $41500.00           $46037.50           $43768.75
+Sweet corn      $48170.01           $52560.75           $50365.38
 
-Enter the crop name: Watermelon
-Enter cost, yield, price per bushel, and increase data: 42.5 160 .40 4.8
+Enter the crop name: Wheat
+Enter cost, yield, price per bushel, and increase data: 43.00 200 0.43 3.1
 			   Minimum Profit      Maximum Profit      Average Profit
-  Watermelon:   $21500.00           $24572.00           $23036.00
+Wheat           $43000.00           $45666.00           $44333.00
 
-Enter the crop name: Carrots
-Enter cost, yield, price per bushel, and increase data: 38 175 .60 7.7
+Enter the crop name: Kale
+Enter cost, yield, price per bushel, and increase data: 43.50 157 0.61 4.3
 			   Minimum Profit      Maximum Profit      Average Profit
-	 Carrots:   $67000.01           $75085.01           $71042.51
+Kale            $52270.01           $56388.12           $54329.06
 
-Enter the crop name: Apples
-Enter cost, yield, price per bushel, and increase data: 40.5 130 .8 2.2
+Enter the crop name: Green Beans
+Enter cost, yield, price per bushel, and increase data: 40.80 118 0.72 2.7
 			   Minimum Profit      Maximum Profit      Average Profit
-	  Apples:   $63500.00           $65788.00           $64644.00
+Green Beans     $44160.01           $46453.93           $45306.97
 
-Old MacDonald, you should plant Carrots
+Old MacDonald, you should plant Kale
 */

b396af68b24e91736e6abd05afde76af197a085d
bissuetox <bissuetox@gmail.com>
2020-01-16 00:51:55 -0800

    Lab IO Final

--- VisualStudio Projects/CPP/CPP/Lab_IO.cpp
+++ VisualStudio Projects/CPP/CPP/Lab_IO.cpp
@@ -1,46 +1,33 @@
 #include <iostream>
+#include <string>
+#define SIZE 100
 using namespace std;
-
-/*
-struct Word {
-	int len = 0;
-	char word[50];
-};
-*/
 
 
 int main() {
-	char sentence[50]; // Initial load char array
-	//Word words[10]; // Array of Word tructs
+	char sentence[SIZE]; // Initial load char array
 	cout << "Enter a sentence: " << endl;
-	cin.get(sentence, 50);
+	cin.get(sentence, SIZE); // Get whole line into sentence array
 
-	int wordNum = 0, wordChar = 0;
-	for (int i = 0; sentence[i] != '\0'; i++) {
-		if (sentence[i] == ' ') {
-			//wordNum++;
-			//wordChar = 0;
-			cout << endl;
+	size_t floatSize = 0; // Keep track of amount of digits after first digit
+	for (int i = 0; sentence[i] != '\0'; i++) { // While end of array is not hit
+		if (sentence[i] == ' ') { // Check if hit delimiter
+			cout << endl; // Print endline
 			continue;
 		}
 
-		else if (sentence[i] >= '0' && sentence[i] <= '9' || sentence[i] == '.'; // don't assume they will type 2...3 or 2,422
+		else if (sentence[i] >= '0' && sentence[i] <= '9' || sentence[i] == '.') { // Check if it is a digit
+			char* fAddy = &sentence[i]; // Set pointer to address of first digit for reference
+			do {
+				i++; // Increase i to continue iterating through the sentence
+				floatSize++; // Add 1 to float size
+			} while (sentence[i] >= '0' && sentence[i] <= '9' || sentence[i] == '.');
+			i--; // Take into account the for loop i++
+			cout << stof(fAddy, &floatSize) * 2; // Print converted float from: first digit to # floatSize of characters after
+		}
 		else {
-			//words[wordNum].word[wordChar] = sentence[i];
-			cout << sentence[i];
-
-			//wordChar++;
-		}
-	}
-
-
-	/*
-	while (sentence[sPos] != '\0') { // While not at end of sentence
-		
-		if (sentence[sPos] != ' ') {
-			words[wPos].word[len] = 
+			cout << sentence[i]; // Print letter normally
 		}
 
 	}
-	*/
 }

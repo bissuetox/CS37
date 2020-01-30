8b2cc2276e8d5d5500e6ed07ebf294a02bbf4b49
bissuetox <bissuetox@gmail.com>
2020-01-29 20:46:58 -0800

    Array Based Stack Final

--- VisualStudio Projects/CPP/CPP/Lab_ArrayBasedStack.cpp
+++ VisualStudio Projects/CPP/CPP/Lab_ArrayBasedStack.cpp
@@ -3,24 +3,25 @@ using namespace std;
 
 class ArrayBasedStack {		// Array Base Stack Class Definition
 public:
-	ArrayBasedStack();		// Member Functions
+	ArrayBasedStack();		// Member Function prototypes
 	void push(int x);
 	int pop();
 	int peek();
+	int isEmpty();
 
 private:
-	const int MaxSize = 10;	// Vars
-	int count;				// Count = position of stack starting at 1, 0 is empty
+	const int MaxSize;		// Variable declarations
+	int count;				// Count = position of top of stack starting at 1, 0 is empty
 	int sArray[10];
 };
 
 ArrayBasedStack::ArrayBasedStack() : MaxSize(10), count(0) {}	// Constructor def and Initializer list - Count set to 0 - empty
 
-void ArrayBasedStack::push(int x) {							// Push Function
+void ArrayBasedStack::push(int x) {							// Push Function definition
 	if (count != MaxSize) {									// If count is not at max size
 		count++;											// Increase count by 1
-		cout << "Count: " << count << endl;					// Debug Display
-		sArray[count - 1] = x;								// Set array pos (count - 1) to x
+		// cout << "Count: " << count << endl;				// Debug Display
+		sArray[count - 1] = x;								// Set array element (count - 1) to x
 		cout << "Pushed: " << sArray[count - 1] << endl;	// Debug Display
 	}
 	else {
@@ -28,21 +29,31 @@ void ArrayBasedStack::push(int x) {							// Push Function
 	}
 }
 
-int ArrayBasedStack::pop() {
-	if (count == 0) {
-		throw exception("Stack is empty!");
+int ArrayBasedStack::pop() {								// Pop Function definition
+	if (count == 0) {										// If array is empty
+		throw exception("Stack is empty!");					// Throw exception with description 
 	}
-	else {
-		cout << "Count: " << count << endl;
-		count--;
-		cout << "Popped: " << sArray[count] << endl;
-		return sArray[count];
+	else {													// If not empty
+		//cout << "Count: " << count << endl;				// Debug Count
+		count--;											// Decrease count by 1 - count now = appropriate element in array
+		cout << "Popped: " << sArray[count] << endl;		// Debug array element
+		return sArray[count];								// Return popped element
 	}
 }
+	
+int ArrayBasedStack::peek() {								// Peek Function definition
+	//cout << "Peek Count: " << count << endl << endl;		// Debug Count
+	return sArray[count - 1];								// Return array element (count - 1)
+}
 
-int ArrayBasedStack::peek() {
-	cout << "Peek Count: " << count << endl;
-	return sArray[count - 1];
+int ArrayBasedStack::isEmpty() {	// Is Empty Function definition
+	if (count == 0) {				// If array is empty
+		return 1;					// Return 1 (true)
+	}
+	else {
+		return 0;					// Return 0 (false)
+	}
+
 }
 
 
@@ -52,10 +63,14 @@ int main() {
 	stack1.push(5);
 	stack1.push(23);
 
-	stack1.pop();
-	stack1.pop();
-	cout << "Peek: " << stack1.peek() << endl;
-	cout << "Peek: " << stack1.peek() << endl;
+	cout << endl;
 
 	stack1.pop();
+	stack1.pop();
+	cout << "Peek: " << stack1.peek() << endl;
+	cout << "Is empty: " << stack1.isEmpty() << endl;
+
+	stack1.pop();
+	cout << "Is empty: " << stack1.isEmpty() << endl;
+	return 0;
 }
